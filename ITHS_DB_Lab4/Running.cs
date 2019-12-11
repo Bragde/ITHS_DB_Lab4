571cefc576dc734f5589f94f86fa633b3f194cca
Daniel Lundholm <Daniel7805@hotmail.com>
2019-12-11 02:15:20 +0100

    before many-many

--- ITHS_DB_Lab4/Running.cs
+++ ITHS_DB_Lab4/Running.cs
@@ -6,6 +6,14 @@ namespace ITHS_DB_Lab4
 {
     class Running : Session
     {
+        public Running()
+        {
+            this.Exercises = new List<Exercise>();
+        }
+        public Running(List<Exercise> exercises)
+        {
+            this.Exercises = exercises;
+        }
         public override List<Exercise> Exercises { get; set; }
         public override List<Gear> Gear { get; set; }
     }
